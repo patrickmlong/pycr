a93fe04697785a6a62348916bb332d59b5a7c35b
Patrick Long <patrick.long@gmail.com>
2020-05-10 20:56:07 -0400

    move data dir to root

--- tests/pycr/test_PcrParser.py
+++ tests/pycr/test_PcrParser.py
@@ -1,9 +1,14 @@
 import pytest
 import pycr.PcrParser as pcr
+import pandas as pd
+import numpy as np
+
+
 
 @pytest.mark.skip(reason="not yet written")
 def test_input_table():
     pass
+
 
 
 @pytest.mark.skip(reason="not yet written")
