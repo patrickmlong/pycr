7fea6aa579b09d5b6bc82b9ba1410f917db5511a
Patrick Long <patrick.long@gmail.com>
2020-05-10 20:25:15 -0400

    setup unit test framework

---
+++ tests/pycr/test_PcrParser.py
@@ -0,0 +1,12 @@
+import pytest
+import pycr.PcrParser as pcr
+
+@pytest.mark.skip(reason="not yet written")
+def test_input_table():
+    pass
+
+
+@pytest.mark.skip(reason="not yet written")
+def test_format_table():
+    pass
+
