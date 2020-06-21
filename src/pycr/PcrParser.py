7be62f543a7644bff9f242759c11a1bf4fd40fa5
Patrick Long <patrick.long@gmail.com>
2020-06-21 09:42:26 -0400

    update pathlib and argparse

--- src/pycr/PcrParser.py
+++ src/pycr/PcrParser.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import os
+from pathlib import Path
+
 
 class PcrParser(object):
     """
@@ -19,21 +20,16 @@ class PcrParser(object):
     
     def input_table(self, file_name):
         try:
-            self.rt_table = pd.read_csv(
-            os.path.normpath(os.path.join(
-            self.file_path, file_name))
-            )
-
+            self.rt_table = pd.read_csv(Path(self.file_path))
         except OSError:
-            print("File {} not found".format(file_name))
+            print(f"File {self.file_path} not found")
             import pdb; pdb.set_trace()
         	
         try:
             self.rt_table = self.rt_table.loc[:,["group", "target", "normalizer"]]
-        	
         except:
             print("Columns: group, target, an/or normalizer not in table " \
-                  "columns: {}".format(self.rt_table.columns))
+                  f"columns:{self.rt_table.columns}")
             import pdb; pdb.set_trace()
                   
       
@@ -50,29 +46,24 @@ class PcrParser(object):
         avg_experimental = \
         self.rt_table[self.rt_table.group == self.experimental].target.mean()
         
-        self.rt_table["average"] = self.rt_table["group"].apply(lambda x: avg_control if x == self.control else avg_experimental)
+        self.rt_table["average"] = self.rt_table["group"]. \
+        apply(lambda x: avg_control if x == self.control else avg_experimental)
 
         self.rt_table["percent_average"] = \
     	(self.rt_table["expression"] / self.rt_table["average"]) *  100
     	 
-        #dd_table = pd.concat([sdf.rt_table[self.rt_table.group == self.experimental], self.rt_table[self.rt_table.group == self.control], axis = 1)
-        #dd_table.columns = ["...."] = \
-        #2** (dd_table.delta_ct_experimental - dd_table.delta_ct_control)
-        
+    
+     def save_table_to_csv(self):
+    
+        self.rt_table.to_csv(f"{Path(self.file_path).parent[0]}" \
+        f"/{Path(self.file_path).stem}_processed.csv"), index = False)        
 
-    def visualize_rt(self):
+
+     def visualize_rt(self):
     
     	sns.set(style = "whitegrid")
     	
     	sns.boxplot(x = "group", y =  "percent_average",
     	            data = self.rt_table)
 
-    
-    def save_table_to_csv(self, file_name):
-    
-        self.rt_table.to_csv(os.path.normpath(
-        os.path.join(self.file_path, file_name)),
-        index = False)
-        
-        
-    
+       
--- src/pycr/runner.py
+++ src/pycr/runner.py
@@ -5,11 +5,7 @@ from pycr.pcrparser import PcrParser
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("file_path", 
-	help = "the path to your raw rna CT data csv file for delta CT RNA level analysis",
-	type = str)
-	
-	parser.add_argument("input_csv", 
-	help = "your raw rna ct data csv file for delta CT RNA level analysis",
+	help = "The path to your raw rna CT data csv file for delta CT RNA level analysis",
 	type = str)
 	
 	parser.add_argument("experimental",
@@ -20,20 +16,14 @@ def main():
 	help = "The name of your control group",
 	type = str)
 	
-	parser.add_argument("output_csv", 
-	help = "csv file name for RNA analysis results",
-	type = str)
-	
     # Run pycr using arguments
 	args = parser.parse_args()
 	
 	rna_parser = PcrParser(args.file_path,args.experimental, args.control)
 						   
-	input_table = rna_parser.input_table(args.input_csv)
-	
+	input_table = rna_parser.input_table()
 	input_table = rna_parser.format_table()
-	
-	rna_parser.save_table_to_csv(args.output_csv)
+	rna_parser.save_table_to_csv()
 	
 	
 if __name__=='__main__':
