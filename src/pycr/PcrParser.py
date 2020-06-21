a364d2b9b9359e47d6a25518c229b82d08e1a1d1
Patrick Long <patrick.long@gmail.com>
2020-06-21 18:41:08 -0400

    update logging

--- src/pycr/PcrParser.py
+++ src/pycr/PcrParser.py
@@ -20,6 +20,8 @@ class PcrParser(object):
         self.experimental = experimental
         self.control = control
         self.rt_table = pd.DataFrame()
+        self.output_path = f"{Path(file_path).parents[0]}" \
+        f"/{Path(file_path).stem}_processed"
 
 
     def input_table(self):
@@ -63,18 +65,18 @@ class PcrParser(object):
 
     def save_table_to_csv(self):
         """Save output file suffixed with "_processed.csv"""
-      
-        output_path = f"{Path(self.file_path).parents[0]}" \
-        f"/{Path(self.file_path).stem}_processed.csv"
-
-        logger.info(f"Saving output table: {output_path}") 
-
-        self.rt_table.to_csv(output_path, index = False)
+        
+        output = self.output_path + ".csv"
+        logger.info(f"Saving output table: {output}")
+        self.rt_table.to_csv(output, index = False)
 
 
     def visualize_rt(self):
         """WIP add visualization method"""
+
+        output = self.output_path +  ".png"
         sns.set(style = "whitegrid")
-    	
-        sns.boxplot(x = "group", y =  "percent_average",
-    	            data = self.rt_table)
+        sns.boxplot(x = "group",
+                    y =  "percent_average",
+                    data = self.rt_table)
+        savefig(output)
