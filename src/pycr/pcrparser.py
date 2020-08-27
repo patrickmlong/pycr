f0c857ed4eb28647d4e7e56a2ddb4f941ec39aee
Patrick <21268383+pkmklong@users.noreply.github.com>
2020-08-27 19:34:37 -0400

    Update pcrparser.py

--- src/pycr/pcrparser.py
+++ src/pycr/pcrparser.py
@@ -38,13 +38,13 @@ class PcrParser(object):
             df = pd.read_csv(Path(self.file_path))
         except OSError:
             print(f"File {self.file_path} not found")
-            import pdb; pdb.set_trace()       
+            #import pdb; pdb.set_trace()       
         try:
             df = df.loc[:,["group", self.normalizer, self.target]]
         except AttributeError:
             logger.info("Columns: group, target, an/or normalizer not in table " \
                   f"columns:{df.columns}")
-            import pdb; pdb.set_trace()
+            #import pdb; pdb.set_trace()
 
         return df
 
