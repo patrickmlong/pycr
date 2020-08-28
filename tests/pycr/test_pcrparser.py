146698869c77d353ce7db9584ee2e3143d4d3f61
Patrick <21268383+pkmklong@users.noreply.github.com>
2020-08-27 20:16:25 -0400

    Update test_pcrparser.py

--- tests/pycr/test_pcrparser.py
+++ tests/pycr/test_pcrparser.py
@@ -20,9 +20,9 @@ def test_check_columns():
         result = p.check_columns(df)
 
 
-def test_format_table():
+def test_calculate_ddct():
     """test final results of format table yield expected"""
 
     p = PcrParser(None, "control", "rpl19", "egf1r")
-    df_result = p.format_table(df_input)
+    df_result = p.calculate_ddct(df_input)
     assert_frame_equal(df_result, df_expected)
