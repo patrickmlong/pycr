9f23dbbe0c4b5f8e435f1cee8fc3a07b56050ed0
Patrick Long <patrick.long@gmail.com>
2020-08-28 08:15:52 -0400

    add black formatting

--- src/pycr/runner.py
+++ src/pycr/runner.py
@@ -4,25 +4,20 @@ from pycr.pcrparser import PcrParser
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("file_path", 
-	help = "The path to ct data (csv) for relative RNA quantification",
-	type = str)
-	
-    parser.add_argument("control",
-	help = "The name of your control group",
-	type = str)
-    parser.add_argument("normalizer",
-	help = "The name of your normalizing reference transcript",
-	type = str)
-    parser.add_argument("target",
-	help = "The name of your target transcript",
-	type = str)
+    parser.add_argument(
+        "file_path",
+        help="The path to ct data (csv) for relative RNA quantification",
+        type=str,
+    )
+
+    parser.add_argument("control", help="The name of your control group", type=str)
+    parser.add_argument(
+        "normalizer", help="The name of your normalizing reference transcript", type=str
+    )
+    parser.add_argument("target", help="The name of your target transcript", type=str)
 
     args = parser.parse_args()
-    rna_parser = PcrParser(args.file_path,
-                           args.control,
-                           args.normalizer,
-                           args.target)
+    rna_parser = PcrParser(args.file_path, args.control, args.normalizer, args.target)
 
     raw_table = rna_parser.load_table()
     rna_parser.check_columns(raw_table)
@@ -31,7 +26,7 @@ def main():
     rna_parser.save_table_to_csv(ddct_table, output_path)
     rna_parser.visualize_rt(ddct_table, output_path)
 
-if __name__=='__main__':
-    """Entry point for console_scripts
-    """
-	main()
+
+if __name__ == "__main__":
+    """Entry point for console_scripts"""
+    main()
