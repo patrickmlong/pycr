03374a5eba98ba6095d94fc2b8736511956b01eb
Patrick <21268383+pkmklong@users.noreply.github.com>
2020-08-27 20:14:58 -0400

    Update runner.py

--- src/pycr/runner.py
+++ src/pycr/runner.py
@@ -25,12 +25,12 @@ def main():
                            args.normalizer,
                            args.target)
 
-    input_table = rna_parser.load_table()
-    rna_parser.check_columns(input_table)
-    formatted_table = rna_parser.format_table(input_table)
+    raw_table = rna_parser.load_table()
+    rna_parser.check_columns(raw_table)
+    ddct_table = rna_parser.calculate_ddct(raw_table)
     output_path = rna_parser.make_output_path()
-    rna_parser.save_table_to_csv(formatted_table, output_path)
-    rna_parser.visualize_rt(formatted_table, output_path)
+    rna_parser.save_table_to_csv(ddct_table, output_path)
+    rna_parser.visualize_rt(ddct_table, output_path)
 
 if __name__=='__main__':
 	main()
