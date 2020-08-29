879bd9bf9153009269e9028ef89b30d070f38729
Patrick Long <patrick.long@gmail.com>
2020-08-28 21:27:42 -0400

    add fixtures

--- tests/pycr/test_runner.py
+++ tests/pycr/test_runner.py
@@ -3,7 +3,9 @@ from pycr.runner import parse_args
 
 
 def test_arg_parse():
-    args = parse_args(["expected_path", "expected_control", "expected_normalizer", "expected_target"])
+    args = parse_args(
+        ["expected_path", "expected_control", "expected_normalizer", "expected_target"]
+    )
     assert args.file_path == "expected_path"
     assert args.control == "expected_control"
     assert args.normalizer == "expected_normalizer"
