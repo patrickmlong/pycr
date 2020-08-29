f65b603dcde38135a0b1e77727972452c0244249
Patrick Long <patrick.long@gmail.com>
2020-08-28 20:45:31 -0400

    black src/pycr/runner.py

--- src/pycr/runner.py
+++ src/pycr/runner.py
@@ -2,13 +2,14 @@ import argparse
 from pycr.pcrparser import PcrParser
 import sys
 
+
 def parse_args(args):
     """Parse command line parameters
     Args:
       args ([str]): command line parameters as list of strings
     Returns:
       :obj:`argparse.Namespace`: command line parameters namespace
-    """    
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "file_path",
@@ -20,9 +21,10 @@ def parse_args(args):
         "normalizer", help="The name of your normalizing reference transcript", type=str
     )
     parser.add_argument("target", help="The name of your target transcript", type=str)
-    
-    return parser.parse_args(args)      
-        
+
+    return parser.parse_args(args)
+
+
 def main(args):
     args = parse_args(args)
 
@@ -35,12 +37,11 @@ def main(args):
     rna_parser.save_table_to_csv(ddct_table, output_path)
     rna_parser.visualize_rt(ddct_table, output_path)
 
-    
+
 def run():
-    """Entry point for console_scripts
-    """
+    """Entry point for console_scripts"""
     main(sys.argv[1:])
 
-    
+
 if __name__ == "__main__":
     run()
