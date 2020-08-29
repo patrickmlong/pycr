1d21fc00ac80250104d79ad74a840f7ee5529aa8
Patrick <21268383+pkmklong@users.noreply.github.com>
2020-08-28 21:23:09 -0400

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,17 @@ conftest.py for pycr.
 import pytest
 from pycr.pcrparser import PcrParser
 
+
+cur_path = f"{os.path.dirname(__file__)}/pycr/data"
+
+
+@pytest.fixture(scope = "session")
+def df_input():
+    return df_input = pd.read_csv(f"{cur_path}/test_data.csv")
+
+def df_expected():
+    return df_expected = pd.read_csv(f"{cur_path}/test_expected.csv")
+
 @pytest.fixture(scope = "session")
 def rna_parser():
 
