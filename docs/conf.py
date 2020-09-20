00a031c027514f7a780a69ec652b16ca0a66a976
Patrick <21268383+pkmklong@users.noreply.github.com>
2020-09-20 12:05:47 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -36,6 +36,7 @@ extensions = ['sphinx.ext.autodoc'
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
@@ -53,6 +54,12 @@ html_theme = 'default'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_logo = ../images/pycr_logo.png"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
 
 master_doc = 'index'
 
